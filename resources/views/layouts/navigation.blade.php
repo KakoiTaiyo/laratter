dad33e467ea975715611db849462923fa9d0cbf0
kakoi <kakoi.taiyo.419@s.kyushu-u.ac.jp>
2024-09-20 20:31:01 +0900

    comment更新、削除機能、tweet検索機能を追加

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,12 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-
                     <x-nav-link :href="route('tweets.index')" :active="request()->routeIs('tweets.index')">
                         {{ __('Tweet一覧') }}
                     </x-nav-link>
                     <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
                         {{ __('Tweet作成') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+                        {{ __('Tweet検索') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -82,6 +84,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
                 {{ __('Tweet作成') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+                {{ __('Tweet検索') }}
             </x-responsive-nav-link>
         </div>
 
